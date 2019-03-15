4634caf7ff7d9b225e34cbb6318e4be03fddf5ec
Stanislav Malyshev <smalyshev@gmail.com>
2019-03-15 13:27:24 -0700

    Port more tests from Wikibase
    Some tests for Elastic functionality are buried within other tests,
    add those to the extension.
    
    Change-Id: I5aa6f3ddbfb468170c7533a241dfcd74786d8fb1

---
+++ tests/phpunit/PropertyHandlerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Wikibase\Search\Elastic\Tests;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\PropertyContent;
+use Wikibase\Repo\Content\PropertyHandler;
+use Wikibase\Repo\Tests\Content\EntityHandlerTestCase;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers \Wikibase\Repo\Content\PropertyHandler
+ * @covers \Wikibase\Repo\Content\EntityHandler
+ *
+ * @group Wikibase
+ * @group WikibaseProperty
+ * @group WikibaseEntity
+ * @group WikibaseEntityHandler
+ *
+ * @license GPL-2.0-or-later
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class PropertyHandlerTest extends EntityHandlerTestCase {
+
+	/**
+	 * @see EntityHandlerTestCase::getModelId
+	 * @return string
+	 */
+	public function getModelId() {
+		return CONTENT_MODEL_WIKIBASE_PROPERTY;
+	}
+
+	/**
+	 * @see EntityHandlerTestCase::contentProvider
+	 */
+	public function contentProvider() {
+		return [];
+	}
+
+	protected function newEntity( EntityId $id = null ) {
+		if ( !$id ) {
+			$id = new PropertyId( 'P7' );
+		}
+
+		$property = Property::newFromType( 'string' );
+		$property->setId( $id );
+		return $property;
+	}
+
+	public function entityIdProvider() {
+		return [
+			[ 'P7' ]
+		];
+	}
+
+	/**
+	 * @param SettingsArray|null $settings
+	 *
+	 * @return PropertyHandler
+	 */
+	protected function getHandler( SettingsArray $settings = null ) {
+		return $this->getWikibaseRepo( $settings )->newPropertyHandler();
+	}
+
+	protected function getTestContent() {
+		$property = new Property( null, null, 'string' );
+		$property->getFingerprint()->setLabel( 'en', 'Kitten' );
+		$property->getStatements()->addNewStatement(
+			new PropertyNoValueSnak( new PropertyId( 'P1' ) )
+		);
+
+		return PropertyContent::newFromProperty( $property );
+	}
+
+	protected function getExpectedSearchIndexFields() {
+		return [ 'label_count', 'statement_count' ];
+	}
+
+	public function testDataForSearchIndex() {
+		$handler = $this->getHandler();
+		$engine = $this->getMock( \SearchEngine::class );
+
+		$page = $this->getMockWikiPage( $handler );
+
+		$data = $handler->getDataForSearchIndex( $page, new \ParserOutput(), $engine );
+		$this->assertSame( 1, $data['label_count'], 'label_count' );
+		$this->assertSame( 1, $data['statement_count'], 'statement_count' );
+	}
+
+}

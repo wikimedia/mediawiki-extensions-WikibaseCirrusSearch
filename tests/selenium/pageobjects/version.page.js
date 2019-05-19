6e638c5d771959ffa2e0d22c1b42dafebf3e786d
Željko Filipin <zeljko.filipin@gmail.com>
2019-05-19 10:52:11 +0200

    Selenium: check if the extension is listed at Special:Version
    Bug: T223774
    Change-Id: I16ecb5c10718595905e15ef49b0c8b91331425da

---
+++ tests/selenium/pageobjects/version.page.js
@@ -0,0 +1,16 @@
+const Page = require( 'wdio-mediawiki/Page' );
+
+// this is just a sample on how to create a page
+class VersionPage extends Page {
+
+	// this is just a sample on how to find an element
+	get extension() { return browser.element( '#mw-version-ext-wikibase-WikibaseCirrusSearch' ); }
+
+	// this is just a sample on how to open a page
+	open() {
+
+		super.openTitle( 'Special:Version' );
+
+	}
+}
+module.exports = new VersionPage();

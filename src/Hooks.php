e11aacd94648ed9de015700adcbc5b2be72fefb3
Matthias Mullie <git@mullie.eu>
2020-12-02 17:16:52 +0100

    Allow license config from messages
    This will allow the config to be maintained by Commons admins,
    who are probably in a better position to do so than staff is.
    
    The code config also still exists, which will allow us to override
    any on-wiki configured map should we ever need to intervene or
    make changes.
    
    Bug: T257938
    Change-Id: I496b67785f766fe72088b16461448653d2a6ffa7

--- src/Hooks.php
+++ src/Hooks.php
@@ -7,6 +7,7 @@ use CirrusSearch\Parser\BasicQueryClassifier;
 use CirrusSearch\Profile\ArrayProfileRepository;
 use CirrusSearch\Profile\SearchProfileRepositoryTransformer;
 use CirrusSearch\Profile\SearchProfileService;
+use CirrusSearch\Util;
 use Language;
 use MediaWiki\MediaWikiServices;
 use RequestContext;
@@ -301,7 +302,26 @@ class Hooks {
 		}
 		$extraFeatures[] = new HasWbStatementFeature( $foreignRepoNames );
 		$extraFeatures[] = new WbStatementQuantityFeature( $foreignRepoNames );
-		$extraFeatures[] = new HasLicenseFeature( $searchConfig->get( 'LicenseMapping' ) );
+
+		// license mapping can come a message, allowing wiki-specific config/overrides,
+		// controlled by users, or in code config (which overrides messages)
+		$licenseMapping = $searchConfig->get( 'LicenseMapping' ) ?: [];
+		$licenseMessage = wfMessage( 'wikibasecirrus-license-mapping' )->inContentLanguage();
+		if ( !$licenseMapping && !$licenseMessage->isDisabled() ) {
+			$lines = Util::parseSettingsInMessage( $licenseMessage->plain() );
+			// reformat lines to allow for whitespace in the license config
+			$joined = implode( "\n", $lines );
+			$stripped = preg_replace( '/\n*?([|,])\n?/', '$1', $joined );
+			$lines = explode( "\n", $stripped );
+			// parse message, add to license mapping
+			foreach ( $lines as $line ) {
+				$data = explode( '|', $line );
+				if ( count( $data ) === 2 ) {
+					$licenseMapping[$data[0]] = array_filter( explode( ',', $data[1] ) );
+				}
+			}
+		}
+		$extraFeatures[] = new HasLicenseFeature( $licenseMapping );
 
 		$languageCodes = WikibaseContentLanguages::getDefaultInstance()
 			->getContentLanguages( 'term' )->getLanguages();

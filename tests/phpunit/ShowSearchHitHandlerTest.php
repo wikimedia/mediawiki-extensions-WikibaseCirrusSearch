da244252310741318f4e26d2991444e4380bbacc
Lucas Werkmeister <mail@lucaswerkmeister.de>
2023-09-23 15:02:31 +0200

    Temporarily skip fallback indicator tests
    Bug: T345883
    Change-Id: I615f0b07bea970e81559d12de89eae9915bdeacd
    Needed-By: I894ef4df05b6c45a8e5f1a53b46ea73cdff6d276

--- tests/phpunit/ShowSearchHitHandlerTest.php
+++ tests/phpunit/ShowSearchHitHandlerTest.php
@@ -314,6 +314,7 @@ class ShowSearchHitHandlerTest extends MediaWikiIntegrationTestCase {
 		$linkCount,
 		$expected
 	) {
+		$this->markTestSkipped( 'T345883' );
 		if ( !ExtensionRegistry::getInstance()->isLoaded( 'CirrusSearch' ) ) {
 			$this->markTestSkipped( 'CirrusSearch not installed, skipping' );
 		}
